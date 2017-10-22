56e99057f25a096a8225a5877de5714bb636b0ec
Scotty Waggoner <ozzieorca@gmail.com>
2017-10-21 19:31:24 -0700

    Add placeholder fields to sync endpoint

--- models/sync.js
+++ models/sync.js
@@ -18,18 +18,76 @@ class Sync {
     if (ctx.body.jwt) {
       ctx.state.user = ctx.body;
 
-      const pair = await crypto.genKeyPair();
+      const securePair = await crypto.genKeyPair();
+      const publicPair = await crypto.genKeyPair();
+
+      await global.db.query(
+        `Update user SET
+        secureS3Bucket = :secureS3Bucket,
+        secureAwsAccessKey = :secureAwsAccessKey,
+        secureAwsSecret = :secureAwsSecret,
+        secureRsaPublicKey = :secureRsaPublicKey,
+        secureRsaPrivateKey = :secureRsaPrivateKey,
+        publicS3Bucket = :publicS3Bucket,
+        publicAwsAccessKey = :publicAwsAccessKey,
+        publicAwsSecret = :publicAwsSecret,
+        publicRsaPublicKey = :publicRsaPublicKey,
+        publicRsaPrivateKey = :publicRsaPrivateKey
+        Where id = :id`,
+        {
+          id: ctx.state.user.id,
+          secureS3Bucket: null, // secureS3Bucket,
+          secureAwsAccessKey: null, // secureAwsAccessKey,
+          secureAwsSecret: null, // secureAwsSecret,
+          secureRsaPublicKey: null, // securePair.publicKey,
+          secureRsaPrivateKey: null, // securePair.privateKey,
+          publicS3Bucket: null, // publicS3Bucket,
+          publicAwsAccessKey: null, // publicAwsAccessKey,
+          publicAwsSecret: null, // publicAwsSecret,
+          publicRsaPublicKey: null, // publicPair.publicKey,
+          publicRsaPrivateKey: null // publicPair.privateKey
+        }
+      );
 
       await list.getLists(ctx);
       const lists = ctx.body; //list.get sets the ctx.body to the list of lists
 
       const [[user]] = await global.db.query(
-        'Select id, uname, privateS3Bucket, privateAwsAccessKey, privateAwsSecret, publicS3Bucket, publicAwsAccessKey, publicAwsSecret, privatePasscode, publicPasscode From user Where id = :id',
+        `Select id,
+        uname,
+        secureS3Bucket,
+        secureAwsAccessKey,
+        secureAwsSecret,
+        securePasscode,
+        secureRsaPublicKey,
+        secureRsaPrivateKey,
+        publicS3Bucket,
+        publicAwsAccessKey,
+        publicAwsSecret,
+        publicPasscode,
+        publicRsaPublicKey,
+        publicRsaPrivateKey
+        From user Where id = :id`,
         {id: ctx.state.user.id}
       );
       ctx.body = {
         lists: lists,
-        user: user
+        secure: {
+          s3Bucket: user.secureS3Bucket,
+          awsAccessKey: user.secureAwsAccessKey,
+          awsSecret: user.secureAwsSecret,
+          passcode: user.securePasscode,
+          rsaPublicKey: user.secureRsaPublicKey,
+          rsaPrivateKey: user.secureRsaPrivateKey
+        },
+        public: {
+          s3Bucket: user.publicS3Bucket,
+          awsAccessKey: user.publicAwsAccessKey,
+          awsSecret: user.publicAwsSecret,
+          passcode: user.publicPasscode,
+          rsaPublicKey: user.publicRsaPublicKey,
+          rsaPrivateKey: user.publicRsaPrivateKey
+        }
       };
     }else{
       ctx.throw(403, 'Not Authorized');
