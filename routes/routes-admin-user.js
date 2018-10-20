78dfe4cbdc14ee590c9a9b54aaccb7595f0c9775
Nathan Bunney <nathan@Inventaweb.net>
2018-10-20 13:29:27 -0700

    routing changes to user and organization

--- routes/routes-admin-user.js
+++ routes/routes-admin-user.js
@@ -6,18 +6,12 @@ const router = require('koa-router')(); // router middleware for koa
 const user = require('../models/user.js');
 
 router.prefix('/user/');
+router.get('/:userID', user.getUser);    // get one user
+router.get('/list', user.getList);       // get all users
+router.post('/register', user.register); // add a user
+router.put('/:userID', user.update);     // update a user
+router.put('/status', user.setStatus);   // update a user status
 
-//router.delete('/user/:userID', user.deleteUser);       // delete a user
-router.get('/:userID', user.getUser);
-router.get('/list', user.getList);        // get all users
-router.post('/register', user.register);
-router.put('/', user.update);    // update a user
-router.put('/status', user.setStatus);    // update a user
-
-//router.get('/user/getroles', user.getRoles);    // get all user roles
-//router.get('/user/getteams', user.getTeams);    // get all user roles
-//router.post('/user/addTeam', user.addTeam);    // update a user
-//router.get('/user/:userID/logo', user.getLogo);
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
 module.exports = router.middleware();
