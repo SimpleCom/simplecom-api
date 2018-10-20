c69bcd4cae68f46890a67731f485501070b68b4f
Nathan Bunney <nathan@Inventaweb.net>
2018-10-20 13:08:52 -0700

    added models for organization CRUD

--- models/organization.js
+++ models/organization.js
@@ -7,28 +7,74 @@
 
 const fs = require('fs-extra');
 const s3 = require("./s3");
+const Return = require('./return');
 
 
 class Organization {
 
   static async addLogo(ctx) {
-    const imageFile = ctx.request.body.files.uploadFile;
-    const origin = ctx.origin;
-    const destination = `logos/${ctx.params.organizationID}/${imageFile.name}`;
-    //await fs.copy(imageFile.path, destination);
+    try {
+      const imageFile = ctx.request.body.files.uploadFile;
+      const origin = ctx.origin;
+      const destination = `logos/${ctx.params.organizationID}/${imageFile.name}`;
+      //await fs.copy(imageFile.path, destination);
 
-    await s3.uploadImage(imageFile.path, destination);
+      await s3.uploadImage(imageFile.path, destination);
 
-    const fileURL = `https://s3.us-west-1.amazonaws.com/simplecom-logos/${destination}`;
+      const fileURL = `https://s3.us-west-1.amazonaws.com/simplecom-logos/${destination}`;
 
-    await global.db.query(`UPDATE organization SET 
-        logo = :logo
-        Where id = :id`,
+      await global.db.query(`UPDATE organization
+                             SET logo = :logo
+                             Where id = :id`,
         {id: ctx.params.organizationID, logo: fileURL}
       );
 
-    const response = {url: fileURL};
-    ctx.body = response;
+      const response = {url: fileURL};
+      ctx.body = Return.setReturn(response);
+    } catch (e) {
+      ctx.body = Return.setReturn(null, false, e);
+    }
+  }
+
+  static async getAll(ctx) {
+    try {
+      const [response] = await global.db.query(`select id, name
+                                                from organization`);
+      ctx.body = Return.setReturn(response);
+    } catch (e) {
+      ctx.body = Return.setReturn(null, false, e);
+    }
+  }
+
+  static async get(ctx) {
+    try {
+      const orgID = ctx.request.params.orgID;
+      const [[response]] = await global.db.query(`select id, name
+                                                from organization
+                                               where id = :orgID`, {orgID: orgID});
+      ctx.body = Return.setReturn(response);
+    } catch (e) {
+      ctx.body = Return.setReturn(null, false, e);
+    }
+  }
+
+
+  static async add(ctx) {
+    try {
+      const response = await global.db.query(`insert into organization (name) values (:name)`, {name: ctx.request.body.name});
+      ctx.body = Return.setReturn(response);
+    } catch (e) {
+      ctx.body = Return.setReturn(null, false, e);
+    }
+  }
+
+  static async update(ctx) {
+    try {
+      const response = await global.db.query(`update organization set name = :name where id = :id`, {name: ctx.request.body.name, id: ctx.request.body.id});
+      ctx.body = Return.setReturn(response);
+    } catch (e) {
+      ctx.body = Return.setReturn(null, false, e);
+    }
   }
 
 }
