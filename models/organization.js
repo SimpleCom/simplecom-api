c8b128baf3beb31315f1d27cab1d40395e3df351
Darin <darindev1000@gmail.com>
2018-10-19 22:57:07 -0700

    Remove getLogo route and function

--- models/organization.js
+++ models/organization.js
@@ -12,7 +12,6 @@ const s3 = require("./s3");
 class Organization {
 
   static async addLogo(ctx) {
-
     const imageFile = ctx.request.body.files.uploadFile;
     const origin = ctx.origin;
     const destination = `logos/${ctx.params.organizationID}/${imageFile.name}`;
@@ -32,26 +31,6 @@ class Organization {
     ctx.body = response;
   }
 
-  static async getLogo(ctx) {
-
-    const imageFile = ctx.request.body.files.uploadFile;
-    const origin = ctx.origin;
-    const destination = `logos/${ctx.params.organizationID}/${imageFile.name}`;
-    //await fs.copy(imageFile.path, destination);
-
-    await s3.uploadImage(imageFile.path, destination);
-
-    const fileURL = `https://s3.us-west-1.amazonaws.com/simplecom-logos/${destination}`;
-
-    await global.db.query(`UPDATE organization SET 
-        logo = :logo
-        Where id = :id`,
-        {id: ctx.params.organizationID, logo: fileURL}
-      );
-
-    const response = {url: fileURL};
-    ctx.body = response;
-  }
 }
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
