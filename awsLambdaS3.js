7b2f7bbdfb88f7a2773496ff95dcfd8dfdf10aa7
Nathan Bunney <nathan@Inventaweb.net>
2018-10-20 08:43:59 -0700

    testing s3 hit

--- awsLambdaS3.js
+++ awsLambdaS3.js
@@ -1,18 +1,23 @@
-// dependencies
-const http = require('http');
+const https = require('https');
 
 exports.handler = function (event, context) {
-  return new Promise((resolve, reject) => {
-    // Read options from the event.
-    console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
-    const srcBucket = event.Records[0].s3.bucket.name;
-    // Object key may have spaces or unicode non-ASCII characters.
-    const srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
-    const options = {
-      host: 'https://api.simplecom.me',
-      path: `/hit/${srcBucket}/${srcKey}`,
-      port: 443,
-      method: 'GET'
-    };
+  // Read options from the event.
+  const srcBucket = event.Records[0].s3.bucket.name;
+  // Object key may have spaces or unicode non-ASCII characters.
+  const srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+  const options = {
+    host: 'api.simplecom.me',
+    path: `/hit/${srcBucket}/${srcKey}`,
+    method: 'GET'
+  };
+  console.log(options);
+  const req = https.request(options, (res) => {
+    var test = res.toString();
+    console.log(JSON.parse(test));
+    return true;
   });
+  if (req){
+    req.write('');
+    req.end();
+  }
 };
